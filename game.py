4882566e28dd22901b05bc7dd913e55e983d7356
8KJS3 <rhwltn3378@gmail.com>
2024-06-01 19:30:10 +0900

    1. player obj create

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
-import random, sys, time
+import random
+import sys
+import time
 import pygame
 
-Display_width = 600
-Display_height = 300
+Display_width = 900
+Display_height = 600
 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -16,11 +18,40 @@ LEFT = 'left'
 RIGHT = 'right'
 
 def init_Game():
-    global Display_surface, clock
+    global Display_surface, clock, player
     pygame.init()
     Display_surface = pygame.display.set_mode((Display_width,Display_height))
     pygame.display.set_caption("2023312005's game")
     clock = pygame.time.Clock()
 
+def run_Game():
+    global Display_surface, clock, player,player_size 
+    
+    #플레이어 크기
+    player_size = 10
+    
+    #플레이어 초기 위치
+    x = Display_width*0.5
+    y = Display_height*0.5
+    
+    Isrun = True
+    while Isrun:
+        for event in pygame.event.get():
+            if event.type in [pygame.QUIT]:
+                pygame.quit()
+                sys.exit()
+                
+                
+        Display_surface.fill(BLACK)
+        drawplayer(x,y)
+        pygame.display.update()
+        clock.tick(60)
+    
+    pygame.quit()
 
-pygame.quit()
+
+def drawplayer(x,y):
+    pygame.draw.circle(Display_surface,WHITE,(x,y),player_size,10)
+
+init_Game()
+run_Game()
